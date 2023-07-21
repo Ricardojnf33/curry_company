4f376ada95e04ef7a2ff4092dbc50d9e342e1d85
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-07-21 19:58:35 -0300

    Update 3_visao_restaurantes.py

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -171,10 +171,11 @@ st.sidebar.markdown( """___""" )
 st.sidebar.markdown( '## Selecione uma data limite' )
 date_slider = st.sidebar.slider( 
     'Até qual valor?',
-    value=pd.datetime( 2022, 4, 13 ),
-    min_value=pd.datetime(2022, 2, 11),
-    max_value=pd.datetime(2022, 4, 6),
-    format='DD-MM-YYYY' )
+    value=pd.Timestamp(2022, 4, 13),
+    min_value=pd.Timestamp(2022, 2, 11),
+    max_value=pd.Timestamp(2022, 4, 6),
+    format='DD-MM-YYYY'
+)
 
 st.header( date_slider )
 st.sidebar.markdown( """___""" )
