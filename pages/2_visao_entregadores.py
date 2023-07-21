6593ab75e6ddc8b2c51b31b74161338090da3f80
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-07-21 19:57:21 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -109,13 +109,16 @@ st.sidebar.markdown( '# Curry Company' )
 st.sidebar.markdown( '### Fastest Delivery in Town' )
 st.sidebar.markdown( """___""" )
 
-st.sidebar.markdown( '## Selecione uma data limite' )
-date_slider = st.sidebar.slider( 
+
+st.sidebar.markdown('## Selecione uma data limite')
+date_slider = st.sidebar.slider(
     'Até qual valor?',
-    value=pd.datetime( 2022, 4, 13 ),
-    min_value=pd.datetime(2022, 2, 11),
-    max_value=pd.datetime(2022, 4, 6),
-    format='DD-MM-YYYY' )
+    value=pd.Timestamp(2022, 4, 13),
+    min_value=pd.Timestamp(2022, 2, 11),
+    max_value=pd.Timestamp(2022, 4, 6),
+    format='DD-MM-YYYY'
+)
+
 
 st.header( date_slider )
 st.sidebar.markdown( """___""" )
